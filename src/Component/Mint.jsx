e9642422f1494dfc08523578fec4775ebeed5e3e
unknown <eklee808@gmail.com>
2022-10-02 13:16:02 +0700

    first commit

--- src/Component/Mint.jsx
+++ src/Component/Mint.jsx
@@ -1,5 +1,5 @@
 import react, {Component, useEffect, useRef,useState} from 'react';
-import Caver from 'caver-js';
+import Web3 from 'web3';
 import axios from 'axios';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
@@ -11,6 +11,8 @@ let nftAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
 const nftABI = ContractData.NFTABI;
 let WLFlag = 0;
 let limit = new Array();
+let web3;
+let contract;
 
 
 export default function Mint(props) {
@@ -21,15 +23,20 @@ export default function Mint(props) {
     const [mintLive, setMintLive] = useState(true);
     const [walletConnection, setWalletConnection] = useState(false);
     const intervals = useRef();
-    let caver = new Caver(window.klaytn);
-    let contract = new caver.contract.create(nftABI, nftAddress);
+    if(process.env.REACT_APP_NETWORK == "goerli"){
+      web3 = new Web3('https://goerli.infura.io/v3/' + process.env.REACT_APP_INFURA_KEY);
+      contract = new web3.eth.Contract(nftABI, nftAddress);
+    }else if(process.env.REACT_APP_NETWORK == "mainnet"){
+      web3 = new Web3('https://mainnet.infura.io/v3/' + process.env.REACT_APP_INFURA_KEY);
+      contract = new web3.eth.Contract(nftABI, nftAddress);
+    }
     let NFTPrice = process.env.REACT_APP_NFT_PRICE.toString();
     let whiteList;
     const currentTimer = async () => {
       let blockTemp;
 
-      if(caver){
-        blockTemp = await caver.klay.getBlockNumber();
+      if(web3){
+        blockTemp = await web3.eth.getBlockNumber();
         setBlockNum(blockTemp.toString());
         if(blockTemp.toString() >= process.env.REACT_APP_START_BLOCK.toString()){
             setMintLive(false);
@@ -58,7 +65,7 @@ export default function Mint(props) {
       })
     }
     let liveDate = process.env.REACT_APP_START_BLOCK;       
-    const timeStmp = + new Date();   
+    const timeStmp = web3.eth.getBlockNumber();   
     if(timeStmp > liveDate){
       setMintLive(false);
     }
@@ -70,18 +77,19 @@ export default function Mint(props) {
 
     async function getData(){
 
-        const [address] = await window.klaytn.enable();      
+        const [address] = await window.ethereum.enable();      
         setWalletConnection(true);
         setAccount(address);
         setMinterAddress(addr);
         whiteListCheck(0);  
     }
     
-    if(window.klaytn){
+    if(window.ethereum){
         getData();
+        console.log(web3);
       
-      window.klaytn.on('accountsChanged', (accounts) => {
-        setAccount(window.klaytn.selectedAddress);
+      window.ethereum.on('accountsChanged', (accounts) => {
+        setAccount(window.ethereum.selectedAddress);
         whiteListCheck(1);
       })    
     }else{
@@ -122,14 +130,15 @@ export default function Mint(props) {
   });
   }
   const connectWallet = async () => {
-    if(!window.klaytn._kaikas.isEnabled()){
-      const [address] = await window.klaytn.enable();
+    console.log(window.ethereum);
+    if(!window.ethereum._metamask.isUnlocked()){
+      const [address] = await window.ethereum.enable();
       setAccount(address);
       setWalletConnection(true);
     }
   }
 
-  const mintNFT = async (cnt) => {
+  const mintNFT = async (cnt) => {//메타마스크는 아래처럼 민트하는게 아니라, bayc_mint 참조해서 민팅해야함
     let ret;    
     whiteListCheck(1);
     let gaslimit = cnt * 850000;
@@ -141,32 +150,40 @@ export default function Mint(props) {
         'Mint number is more than max count. 최대발행갯수보다 많은 수의 발행을 시도하였습니다.',
       );
     }
+    console.log("AA");
     if(process.env.REACT_APP_WHITELIST == "true" && WLFlag != 1){
       alert("해당 주소는 민팅 대상 화이트리스트에 포함되어있지 않습니다.");
     }else {
       if(process.env.REACT_APP_WHITELIST == "true"){   
-          ret = await caver.klay.sendTransaction({
-              type: 'SMART_CONTRACT_EXECUTION',
-              from: account,
-              to: nftAddress,
-              value: caver.utils.toPeb((NFTPrice * cnt).toString(), 'KLAY'),
-              data: contract.methods.mintMultiple(account,cnt).encodeABI(),
-              gas: gaslimit
-            }).then(async (res)=>{
-              console.log(res);
-            })
-            .catch((err) => {alert("Mint has failed.");});    
+        const transactionParameters = {
+          nonce: '0x00', // ignored by MetaMask
+          to: nftAddress, // Required except during contract publications.
+          from: window.ethereum.selectedAddress, // must match user's active address.
+        value: parseInt(web3.utils.toWei((process.env.REACT_APP_NFT_PRICE*cnt).toString(), 'ether')).toString(16), // Only required to send ether to the recipient from the initiating external account.
+          data: contract.methods.mintMultiple(account,cnt).encodeABI(), // Optional, but used for defining smart contract creation and interaction.
+        };
+        
+        const txHash = await window.ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [transactionParameters],
+        });
+        mintCount = await contract.methods.totalSupply().call();
+        setMintCnt(mintCount);
+      
+        await wait(3000);
       }else{
-        ret = await caver.klay.sendTransaction({
-            type: 'SMART_CONTRACT_EXECUTION',
-            from: account,
-            to: nftAddress,
-            value: caver.utils.toPeb((NFTPrice * cnt).toString(), 'KLAY'),
-            data: contract.methods.mintMultiple(account,cnt).encodeABI(),
-            gas: gaslimit
-          }).then((res)=>{console.log(res);})
-          .catch((err) => {alert("Mint has failed.");});
-      }
+        const transactionParameters = {
+          nonce: '0x00', // ignored by MetaMask
+          to: nftAddress, // Required except during contract publications.
+          from: window.ethereum.selectedAddress, // must match user's active address.
+        value: parseInt(web3.utils.toWei((process.env.REACT_APP_NFT_PRICE*cnt).toString(), 'ether')).toString(16), // Only required to send ether to the recipient from the initiating external account.
+          data: contract.methods.mintMultiple(account,cnt).encodeABI(), // Optional, but used for defining smart contract creation and interaction.
+        };
+        
+        const txHash = await window.ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [transactionParameters],
+        });
         mintCount = await contract.methods.totalSupply().call();
         setMintCnt(mintCount);
       
@@ -174,6 +191,7 @@ export default function Mint(props) {
     }
     
   }
+}
   
 const bull = (
   <Box
@@ -200,4 +218,3 @@ const bull = (
     </div>
   );
 }
-
